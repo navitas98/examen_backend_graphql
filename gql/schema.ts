64dc9fbf1f814976c50632fed87646ac827953ce
jnavas98 <j.navasdam@outlook.es>
2024-01-19 09:47:34 +0100

    prueba 2

--- gql/schema.ts
+++ gql/schema.ts
@@ -1,6 +1,8 @@
+//Quitar mongoose de deno.json si el codigo no hace conexion a base de datos
 export const typeDefs = `#graphql
 type Query{
 test:String!
+hola:String!
 }
 type Mutation{
 test:String!
